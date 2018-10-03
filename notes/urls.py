0200206670f96b28e2aea5e81ff9b1098a6739aa
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-03 19:19:47 +0300

    add requirements.txt file to gitignore

--- notes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url  # import the url function from the from the django.conf.urls
-from . import views  # import the app's views module.
-from django.conf import settings
-
-urlpatterns=[
-    url ( r'^$' , views.home , name='home' ) ,
-]
